5978b4e9d15a6cca7c9024b416bbcfefd5ec9b7b
Marcelo Rossini <mar.18.celo@gmail.com>
2020-02-17 16:55:57 -0300

    upload

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import './style.css';
 
+import code from '../../assets/services/code.svg';
+import management from '../../assets/services/management.svg';
+import pen from '../../assets/services/pen.svg';
+import social from '../../assets/services/social.svg';
+import promote from '../../assets/services/promote.svg';
+
 export default () => {
   return (
     <div className="services">
       <h1>Serviços</h1>
+      <ul className="items">
+        <li>
+          <img src={management}/>
+          <strong>Marketing Digital</strong>
+        </li>
+        <li>
+          <img src={pen}/>
+          <strong>Identidade Visual</strong>
+        </li>
+        <li>
+          <img src={social}/>
+          <strong>Gerenciamento de Mídias Sociais</strong>
+        </li>
+        <li>
+          <img src={promote}/>
+          <strong>Impressos</strong>
+        </li>    
+        <li>
+          <img src={code}/>
+          <strong>Desenvolvimento de Sites</strong>
+        </li>            
+      </ul>
     </div>
   );
 }
