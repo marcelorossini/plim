9187ee29906ad105a1c279304ed41a9408e6060d
Marcelo Rossini <marcelo@Marcelos-iMac.local>
2020-02-18 12:28:55 -0300

    upload

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,11 +7,11 @@ export default () => {
     <div className="home">
       <h1>Soluções Criativas</h1>
       <p>
-        Fusce ut ante ultrices, cursus purus vel, congue velit. Nam in rutrum arcu. Sed id massa a leo
-        rutrum
-        accumsan eget id mauris. Fusce vitae egestas felis. Nunc molestie urna ex, sit amet facilisis
-        lorem
-        varius aliquet. Aliquam erat volutpat. Integer eget ultricies mauris, quis vestibulum metus.
+      Está precisando divulgar seu trabalho mas não sabe por onde começar?<br/>A gente tem a solução! ;)<br/>
+      <br/>
+      Somos especializados em técnicas de marketing digital para alavancar seus rendimentos, estando presente tanto na criação de sua identidade visual quanto na divulgação de suas campanhas, garantindo o que há de mais atual na área das mídias digitais.<br/>
+      <br/>
+      Aqui não falta criatividade, por isso, atendemos desde pequenos negócios até grandes empreendimentos, inclusive, abrangendo a criação de impressos gráficos e o desenvolvimento de sites.
       </p>
       <button>SAIBA MAIS</button>
       <img className="image" src={home} />
