45ceaeb2d04ecd144b10ad8eda9c8e64c56e51f2
Marcelo Rossini <marcelorossini@Marcelos-iMac.local>
2020-06-08 11:30:17 -0300

    Teste

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -17,17 +17,17 @@ export default () => {
                     <li>
                         <img src={socialMedia} />
                         <strong>Gestão de Redes Sociais</strong>
-                        <p>aaaaaaaa aaa</p>
+                        <p>Marcar presença na internet é fundamental para o sucesso do seu negócio, por isso, a gestão das suas redes sociais xxxxxx xxxxx xxxxxxx xxxxx xx xxxxx xxxxx xxxxx . </p>
                     </li>
                     <li>
                         <img src={marketing} />
                         <strong>Marketing Digital</strong>
-                        <p>aaaaaaaa aaa</p>
+                        <p>Encontrar o público certo e descobrir a melhor maneira de atraí-los é uma tarefa que exige muita experiência prática . </p>
                     </li>
                     <li>
                         <img src={bussinessCard} />
                         <strong>Cartões de Visita</strong>
-                        <p>aaaaaaaa aaa</p>
+                        <p>Nada mais deselegante que entregar seu contato em um pedaço de papel rasgado, não é mesmo? Os cartões de visita.</p>
                     </li>
                     <li>
                         <img src={tablet} />
