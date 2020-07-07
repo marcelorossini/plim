4bef61132fed3324961d83b1a5599080151af4f1
Marcelo Rossini <marcelorossini@MacBook-Air-de-Marcelo.local>
2020-07-07 16:49:54 -0700

    update

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -11,21 +11,45 @@ export default () => {
             <div className="wrapper">
                 <h1 className="mark-right">SOBRE A PLIM</h1>
                 <div className="content">
-                    <div className="grid">                                       
-                        <div className="plim">
-                            <div>
-                                <img src={logo} alt=""/>
-                            </div>      
-                            <p>
-                            
-                            </p>                      
+                    <div className="plim">
+                        <div>
+                            <img src={logo} alt="" />
                         </div>
+                        <p>
+                            Atuando no ramo desde o ano de 2016, a Plim se consolidou no mercado digital por seu modelo jovial e ousado, trazendo como pilares fundamentais a qualidade, agilidade e criatividade.
+                            O segredo do reconhecimento e do prestígio profissional está na responsabilidade e transparência, além de muito carinho pelo ofício, fato que torna cada trabalho único e especial.
+                        </p>
+                        <div className="people">
+                            <div className="image"><img src={imgAryane} alt="" /></div>
+                            <div className="info">
+                                <strong>Aryane Toffetti</strong>
+                                <p>
+                                    Designer - CEO
+                                </p>
+                            </div>
+                            <div className="image"><img src={imgMarcelo} alt="" /></div>
+                            <div className="info">
+                                <strong>Marcelo Rossini</strong>
+                                <p>
+                                    Desenvolvedor - Parceiro
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom"></div>
+        </section>
+    )
+}
+
+/*
                         <div className="people">
                             <div className="image"><img src={imgAryane} alt=""/></div>
                             <div className="info">
                                 <strong>Aryane Toffetti</strong>
                                 <p>
-                                    Designer - CEO                                
+                                    Designer - CEO
                                 </p>
                             </div>
                             <div className="image"><img src={imgMarcelo} alt=""/></div>
@@ -33,17 +57,7 @@ export default () => {
                                 <strong>Marcelo Rossini</strong>
                                 <p>
                                     Desenvolvedor - Parceiro
-                                </p>                            
+                                </p>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>            
-            <div className="bottom"></div>            
-        </section>
-    )
-}
-
-/*
-
 */
